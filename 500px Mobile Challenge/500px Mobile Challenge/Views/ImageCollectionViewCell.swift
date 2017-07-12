524a585e4136df17610953c0379b46d5e6a31dfe
Raymond Kam <raymondhmkam@gmail.com>
2017-07-12 17:37:58 -0400

    Created new iOS application project. Added collection view controller as initial view controller and created a collection view cell with an image view.

---
+++ 500px Mobile Challenge/500px Mobile Challenge/Views/ImageCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCollectionViewCell.swift
+//  500px Mobile Challenge
+//
+//  Created by Raymond Kam on 2017-07-12.
+//  Copyright © 2017 Raymond Kam. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
